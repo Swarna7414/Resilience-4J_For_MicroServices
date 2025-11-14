b27e23c86f05531f6efa79c38469351c7f89fb88
Swarna7414 <swarnasaisankar044@gmail.com>
2025-11-13 19:49:59 -0600

    added another folder

--- ServiceA/src/main/java/com/MicroA/ServiceA/Controller/ServiceControllerA.java
+++ ServiceA/src/main/java/com/MicroA/ServiceA/Controller/ServiceControllerA.java
@@ -1,45 +1,52 @@
 package com.MicroA.ServiceA.Controller;
 
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+
 
 @RestController
 @RequestMapping("/serviceA")
 public class ServiceControllerA {
 
-    int hit =0;
-
-    int hit2=0;
+    private Integer hit;
 
     private static final Logger logger = LoggerFactory.getLogger(ServiceControllerA.class);
 
     @GetMapping("/get")
-    public Map<String, Object> hitting(){
+    public Map<String, Object> getFirst(String name,Integer age){
+        Integer hitM=0;
         hit++;
-        logger.info("hit number {} occured",hit);
+        hitM++;
+        logger.info("THis is the Hit one {} with method hit {} , with name {}, age {}",hit++,hitM,name,age);
         return Map.of(
-                "Message","PUka",
-                "status", HttpStatus.OK,
-                "malli","Hit Ayindi"
+                "name",name,
+                "age",age
         );
+    }
+
+    @GetMapping("/getparam")
+    public String hitWithParam(@RequestParam String name){
+        Integer hitM=0;
+        hit++;
+        hitM++;
+        logger.info("THis is the Hitone {} with method hit {} with name {}",hit,hitM,name);
+        return "Method Hit ayindi ra pumka "+name;
     }
 
     @PostMapping("/post")
-    public Map<String, Object> hitWithPost(@RequestParam String message) throws InterruptedException {
-
-//        Thread thread = new Thread();
-//        thread.sleep(50_000L);
-
-        hit2++;
-        logger.info("Hit number {} occured",hit2);
-        return Map.of(
-                "message",message,
-                "status",HttpStatus.OK.value()
-        );
+    public ResponseEntity<Map<String, Object>> getPost(@RequestBody Map<String, Object> map){
+        Integer hitM=0;
+        hit++;
+        hitM++;
+        logger.info("This is the Hit one {} with method hit {} with map {}",hit,hitM,map);
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
+
 }
